cf43e8b316adbe32e1f90154db12c79cf55a5764
SamedovRasul <samedovrasul7@gmail.com>
2023-03-05 11:59:44 +0400

    change uri

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/', (req,res) =>{
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen((PORT), () => console.log(`server running on Port:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`))
